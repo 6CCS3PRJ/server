a61719700c2f012785e32c48cbf0434871aebe88
Danilo Del Busso <danilo.delbusso1@gmail.com>
2021-02-25 13:18:50 +0100

    Update swagger options

--- src/swagger.js
+++ src/swagger.js
@@ -1,25 +1,28 @@
 let options = {
     swaggerDefinition: {
         info: {
-            description: "server APi docs",
-            title: "DEC App API",
+            description: "server API docs",
+            title: "Contact Tracing API",
             version: "0.0.1",
         },
-        host: "localhost:5000",
-        basePath: "/api",
-        produces: ["application/json", "application/xml"],
-        schemes: ["http", "https"],
+        host: 'localhost:4683',
+        basePath: '/api/v1',
+        produces: [
+            "application/json",
+            "application/xml"
+        ],
+        schemes: ['http', 'https'],
         securityDefinitions: {
             JWT: {
-                type: "apiKey",
-                in: "header",
-                name: "Authorization",
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
                 description: "",
-            },
-        },
+            }
+        }
     },
     basedir: __dirname, //app absolute path
-    files: ["./routes/**/*.js"], //Path to the API handle folder
+    files: ['./routes/**/*.js'] //Path to the API handle folder
 };
 
 module.exports = {
